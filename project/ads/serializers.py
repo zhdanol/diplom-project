bc0500df34fea2c47d529d63865ace4ce90c28f0
zhdanol <olegzdanov87@gmail.com>
2025-11-15 01:36:32 +0700

    add ser

---
+++ project/ads/serializers.py
@@ -0,0 +1,119 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from ads.models import Category, Shop, Product, ProductInfo, User, OrderItem, Order, ProductParameter, Contact
+
+class UserLoginSer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True)
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+        
+        if email and password:
+            user = authenticate(username=email, password=password)
+            if not user:
+                raise serializers.ValidationError('Неверные данные')
+        else:
+            raise serializers.ValidationError('Необходимо указать email и пароль')
+        
+        attrs['user'] = user
+        return attrs
+
+
+
+class ContactSer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = ['id', 'country', 'region', 'city', 'street', 'house', 'building', 'structure', 'apartment', 'phone', 'user']
+        
+class UserSer(serializers.ModelSerializer):
+    contacts = ContactSer(read_only=True)        
+    
+    class Meta:
+        model = Category
+        fields = ['id', 'first_name', 'last_name', 'email', 'company', 'position']
+        
+class UserRegisterSer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    password_confirm = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'password', 'password_confirm']
+        def validate(self, attrs):
+            if attrs['password'] != attrs['password_confirm']:
+                raise serializers.ValidationError('Пароли не совпадают')
+            return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name',''),
+            last_name=validated_data.get('last_name','')
+            type = 'buyer'
+        )
+        return user
+    
+
+class ShopSer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Shop
+        fields = ['id', 'name', 'state']
+
+class CategorySer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
+class ProductSer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
+    
+    class Meta:
+        model = Product
+        fields = ['name', 'category']
+        
+class ProductParameterSer(serializers.ModelSerializer):
+    parameter = serializers.StringRelatedField()
+    
+    class Meta:
+        model = ProductParameter
+        fields = ['parameter', 'value']
+
+class ProductInfoSer(serializers.ModelSerializer):
+    product = ProductSer(read_only=True)
+    parameters = ProductParameterSer(read_only=True, many=True)
+    shop = ShopSer(read_only=True)
+    
+    class Meta:
+        model = ProductInfo
+        fields = ['id', 'product', 'shop', 'model', 'price', 'price_rrc', 'quantity', 'parameters']
+        
+class OrderItemSer(serializers.ModelSerializer):
+    total_price = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product_info', 'quantity', 'total_price']
+    
+    def total_price(self, obj):
+        return obj.quantity * obj.product_info.price
+    
+class OrderItemCreateSer(OrderItemSer):
+    product_info = ProductInfoSer(read_only=True)
+    
+class OrderSer(serializers.ModelSerializer):
+    ordered_items = OrderItemCreateSer(read_only=True, many=True)
+    
+    total_sum = serializers.IntegerField()
+    contact = ContactSer(read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'ordered_items', 'state', 'total_sum', 'contact']
+        
+    def get_total_sum(self, obj):
+        return sum(item.quantity*item.product_info.price for item in obj.ordered_items.all())
