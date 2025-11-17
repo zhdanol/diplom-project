848ee8df5dd97d7c287ec9d85bfe23acad80aa96
zhdanol <olegzdanov87@gmail.com>
2025-11-18 00:19:23 +0700

    add models

--- project/ads/models.py
+++ project/ads/models.py
@@ -1,19 +1,21 @@
 from django.db import models
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from django_rest_passwordreset.tokens import get_token_generator
 
 STATE_CHOICES = (
-    ('cart_status', 'Статус корзины')
-    ('new', 'Новый')
-    ('confirmed', 'Подтверждён')
-    ('collected', 'Собран')
-    ('sent', 'Собран')
-    ('delivered', 'Доставлен')
+    ('cart', 'Корзина'),
+    ('new', 'Новый'),
+    ('confirmed', 'Подтверждён'),
+    ('assembled', 'Собран'),
+    ('sent', 'Отправлен'),
+    ('delivered', 'Доставлен'),
     ('canceled', 'Отменён')
 )
 
 USER_TYPE_CHOICES = (
-    ('shop', 'Магазин')
+    ('shop', 'Магазин'),
     ('buyer', 'Покупатель')
     
 )
@@ -72,7 +74,8 @@ class User(AbstractUser):
 class Shop(models.Model):
     name = models.CharField(max_length=40, verbose_name='Название')
     url = models.URLField(verbose_name='Ссылка', null=True, blank=True)
-    
+    user = models.OneToOneField(User, verbose_name='Пользователь', blank=True, null=True, on_delete=models.CASCADE, related_name='shop')
+    state = models.BooleanField(verbose_name='Статус получения заказов', default=True)
     class Meta:
         verbose_name = 'Магазин'
         verbose_name_plural = 'Список магазинов'
@@ -81,7 +84,23 @@ class Shop(models.Model):
     def __str__(self):
         return self.name
 
-
+class ConfirmEmailToken(models.Model):
+    user = models.ForeignKey(User, related_name='confirm_email_tokens', on_delete=models.CASCADE, verbose_name=('Пользователь'))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=('Дата создания'))
+    key = models.CharField(('key'), max_length=65, db_index=True, unique=True)
+    
+    class Meta:
+        verbose_name = 'Токен подтверждения Email'
+        verbose_name_plural = 'Токены подтверждения Email'
+        
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = get_token_generator().generate_token()
+        return super().save(*args, **kwargs)
+    
+    def __str__(self):
+         return f'Токен для {self.user}'
+        
 class Category(models.Model):
     name = models.CharField(max_length=40, verbose_name='Название')
     shops = models.ManyToManyField(Shop, verbose_name='Магазины', related_name='categories', blank=True)
@@ -114,7 +133,7 @@ class ProductInfo(models.Model):
     shop = models.ForeignKey(Shop, verbose_name='Магазин', related_name='product_infos', blank=True, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name='Количество')
     price = models.PositiveIntegerField(verbose_name='Цена')
-    price_rrc = models.PositiveIntegerField(verbose_name='Розичная цена')
+    price_rrc = models.PositiveIntegerField(verbose_name='Розничная цена')
     
     class Meta:
         verbose_name = 'Имя продукта'
@@ -175,19 +194,93 @@ class OrderItem(models.Model):
     class Meta:
         verbose_name = 'Заказанная позиция'
         verbose_name_plural = 'Список заказанных позиций'
-        constraints = [models.UniqueConstraint(fields=['order_id', 'product_info'], name='unique_order_item')]   
+        constraints = [
+            models.UniqueConstraint(fields=['order', 'product'], name='unique_order_item')
+            ]   
 
 
 class Contact(models.Model):
     type = models.CharField(verbose_name='Тип контакта', max_length=20, choices=CONTACT_TYPE, default='phone')
-    user = models.ForeignKey(verbose_name='Пользователь', related_name='contacts', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, verbose_name='Пользователь', related_name='contacts', on_delete=models.CASCADE)
+    country = models.CharField(verbose_name='Страна', max_length=50, blank=True, null=True)
+    region = models.CharField(verbose_name='Регион', max_length=50, blank=True, null=True)
+    city = models.CharField(verbose_name='Город', max_length=50, blank=True, null=True)
+    street = models.CharField(verbose_name='Улица', max_length=100, blank=True, null=True)
+    house = models.CharField(verbose_name='Дом', max_length=10, blank=True, null=True)
+    building = models.CharField(verbose_name='Корпус/Строение', max_length=10, blank=True, null=True)
+    structure = models.CharField(verbose_name='Строение', max_length=10, blank=True, null=True)
+    apartment = models.CharField(verbose_name='Квартира', max_length=10, blank=True, null=True)
+    phone = models.CharField(verbose_name='Телефон', max_length=20, blank=True, null=True)
     value = models.CharField(verbose_name='Значение', max_length=150)
+    
+    is_main = models.BooleanField(verbose_name='Основной контакт', default=False)
+    
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
     
     class Meta:
         verbose_name = 'Контакт'
         verbose_name_plural = 'Контакты'
         constraints = [models.UniqueConstraint(fields=['user', 'type', 'value'], name='unique_user_contact')]
+        ordering = ['-is_main', 'type']
+        
         
     def __str__(self):
-        return f'{self.user}: {self.value}'
+        return f'{self.user.email}: {self.get_type_display()} - {self.get_display_value()}'
     
+    def get_display_value(self):
+        if self.type == 'address':
+            return self.get_full_address()
+        elif self.type == 'phone':
+            return self.phone or self.value
+        else:
+            return self.value
+    
+    def get_full_address(self):
+        address_parts = []
+        
+        if self.country:
+            address_parts.append(self.country)
+        if self.region:
+            address_parts.append(self.region)
+        if self.city:
+            address_parts.append(f'Г. {self.city}')
+        if self.street:
+            address_parts.append(f'Ул. {self.street}')
+            
+        building_parts = []
+
+        if self.house:
+            address_parts.append(f'Д. {self.house}')
+        if self.building:
+            address_parts.append(f'Корп. {self.building}')
+        if self.structure:
+            address_parts.append(f'Стр. {self.structure}')
+        if self.apartment:
+            address_parts.append(f'Кв. {self.apartment}')
+            
+        if building_parts:
+            address_parts.append(', '.join(building_parts))
+            
+        return ', '.join(address_parts)
+    
+    def clean(self):
+        if self.type == 'phone' and not (self.phone or self.value):
+            raise ValidationError('Необъходимо указать телефон')
+        if self.type == 'email' and not (self.phone or self.value):
+            raise ValidationError('Необъходимо указать email')
+        if self.type == 'address' and not any([self.city, self.street, self.house]):
+            raise ValidationError('Необъходимо указать город, улицу и дом')
+    
+    def save(self, *args, **kwargs):
+        if self.is_main:
+            Contact.objects.filter(
+                user=self.user,
+                type=self.type,
+                is_main=True
+            ).exclude(pk=self.pk).update(is_main=False)
+            
+        if self.type == 'address' and not self.value:
+            self.value = self.get_full_address()
+        
+        super().save(*args, **kwargs)
